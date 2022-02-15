f4353c8ecf0ad9072a045180449eb9150ed582bb
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-02-15 16:16:24 +0530

    extract frame difference

--- working1.py
+++ working1.py
@@ -7,23 +7,26 @@ import numpy as np
 p_frame_thresh= 360000 # modify constant to make more accurate
 p_frame_count= 0
 
-vid= cv2.VideoCapture('output2.mkv')
+# input3.mkv is SPM sir class
+vid= cv2.VideoCapture('input3.mkv')
 
 success, prev_frame= vid.read()
+f= open('frame.txt', 'w')
 
 while success:
     success, curr_frame= vid.read()
     if success:
         diff= cv2.absdiff(curr_frame, prev_frame)
         non_zero_count= np.count_nonzero(diff)
+        f.write(str(non_zero_count)+'\n')
         if non_zero_count > p_frame_thresh:
             p_frame_count+= 1
-            print("Got P-Frame", p_frame_count)
-            cv2.imshow('frame', curr_frame)
-            if cv2.waitKey(30) & 0xFF == ord('q'):
-                break
+            # print("Got P-Frame", p_frame_count)
+            # cv2.imshow('frame', curr_frame)
+            # if cv2.waitKey(0) & 0xFF == ord('q'):
+                # break
 
         prev_frame= curr_frame
 
 vid.release()
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
