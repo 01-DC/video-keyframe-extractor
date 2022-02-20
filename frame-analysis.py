0a45acb4191116a30d6072bc15f8407412cd5b6e
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-02-20 14:36:40 +0530

    post analysis SPM class rec

--- frame-analysis.py
+++ frame-analysis.py
@@ -1,8 +1,11 @@
+# Script for analysing the comparison value and to determine threshold
+
 from cv2 import mean
 from matplotlib import pyplot as plt
 import statistics
 
-f= open('frame2.txt', 'r')
+# f= open('frame2.txt', 'r')
+f= open('frame3.txt', 'r') # for SPM Slides
 
 all_data= list(map(int, f.read().strip().split()))
 
@@ -10,5 +13,10 @@ plt.plot(all_data)
 
 plt.show()
 
+all_data.sort()
+
+upper_all_data= all_data[len(all_data)//2:]
+
 print(statistics.mean(all_data))
+print(statistics.mean(upper_all_data))
 print(statistics.median(all_data))
