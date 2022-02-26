9821f081ed65254b0dfd82af507b88a51a194ec2
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-02-26 15:22:19 +0530

    added progress bar

--- videoAnalysis.py
+++ videoAnalysis.py
@@ -3,9 +3,18 @@ import numpy as np
 from PIL import Image
 import imagehash
 
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = '\r')
+    if iteration == total: 
+        print()
+
 def videoAnalysis(FRAMES_SKIP, VIDEO_PATH):
     vid= cv2.VideoCapture(VIDEO_PATH)
     f= open('frameHashData.txt', 'w', buffering=1) # For storing and analysing frame hash differences
+    printProgressBar(vid.get(cv2.CAP_PROP_POS_FRAMES), vid.get(cv2.CAP_PROP_FRAME_COUNT), prefix = 'Progress:', suffix = 'Complete', length = 50)
 
     success, prevImg= vid.read()
     prevHash= imagehash.dhash(Image.fromarray(np.uint8(prevImg)).convert('RGB'), hash_size=64)
@@ -22,10 +31,9 @@ def videoAnalysis(FRAMES_SKIP, VIDEO_PATH):
             break
 
         currHash= imagehash.dhash(Image.fromarray(np.uint8(currImg)).convert('RGB'), hash_size=64)
-
         f.write(str(currHash-prevHash)+' '+str(vid.get(cv2.CAP_PROP_POS_FRAMES))+'\n')
-        
         prevHash= currHash
+        printProgressBar(vid.get(cv2.CAP_PROP_POS_FRAMES), vid.get(cv2.CAP_PROP_FRAME_COUNT), prefix = 'Progress:', suffix = 'Complete', length = 50)
 
     f.close()
     vid.release()
