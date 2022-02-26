3505bfc82251e298c380dcdde4e9bc37571d0d35
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-02-26 17:37:42 +0530

    recommended settings

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import frameSelector
 import frameExtractor
 import makePdf
 
-FRAMES_SKIP= 10 # Increase value for faster processing but may lead to skipped slides
+FRAMES_SKIP= 30 # Increase value for faster processing but may lead to skipped slides
 FOLDER_PATH= os.path.join(os.getcwd(), 'ExtractedSlides') # Folder where slide images and merged pdf will be stored
 ## EDIT THIS BEFORE RUNNING
-VIDEO_PATH= "F:/Class Recordings/2022-01-28 11-33-05.mkv" # Absolute path of video for running script using only / as separator
+VIDEO_PATH= "F:/Class Recordings/2022-01-31 14-33-43.mkv" # Absolute path of video for running script using only / as separator
 
 videoAnalysis.videoAnalysis(FRAMES_SKIP, VIDEO_PATH)
 selectedFrames= frameSelector.frameSelector()
