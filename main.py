54d36bb32d2856567bcdf7db2daf710e2c38c1da
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-02-26 15:28:16 +0530

    recommended frame skip

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import frameSelector
 import frameExtractor
 import makePdf
 
-FRAMES_SKIP= 10 # Increase value for faster processing but may lead to skipped slides
+FRAMES_SKIP= 30 # Increase value for faster processing but may lead to skipped slides
 FOLDER_PATH= os.path.join(os.getcwd(), 'ExtractedSlides') # Folder where slide images and merged pdf will be stored
 ## EDIT THIS BEFORE RUNNING
 VIDEO_PATH= "F:/Class Recordings/2022-01-24 14-37-41.mkv" # Absolute path of video for running script
